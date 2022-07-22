4ff71cdfc8d08159147b4d7fc7b7cd47c9e1ed76
jnadori <jakab.nadori@lidl.hu>
2022-07-22 14:51:02 +0200

    updated test

--- test_main.py
+++ test_main.py
@@ -11,11 +11,15 @@ def test_root():
     assert r.json() ==  {"message": "Welcome to Your Sentiment Classification FastAPI"}
     
 def test_predict_salary_high():
-    r = client.post("/predict", json = {"model_path":"model/model.pkl","inout_data":"{'age': 50,'workclass': 'Self-emp-not-inc','fnlgt': 83311,'education': 'Bachelors','education-num': 13,'marital-status': 'Married-civ-spouse','occupation': 'Exec-managerial','relationship': 'Husband','race': 'White','sex': 'Male','capital-gain': 0,'capital-loss': 0,'hours-per-week': 13,'native-country': 'United-States','salary': '<=50K'}"})
+    r = client.post("/predict", json = {
+  "model_path": "model/model.pkl",
+  "input_data": "{'age': 50,'workclass': 'Self-emp-not-inc','fnlgt': 83311,'education': 'Bachelors','education-num': 13,'marital-status': 'Married-civ-spouse','occupation': 'Exec-managerial','relationship': 'Husband','race': 'White','sex': 'Male','capital-gain': 0,'capital-loss': 0,'hours-per-week': 13,'native-country': 'United-States','salary': '<=50K'}"
+})
     assert r.status_code == 200
     assert r.json() == {"salary": 0}
     
 def test_predict_salary_low():
-    r = client.post("/predict", json = {"model_path":"model/model.pkl","inout_data":"{'age': 52, 'workclass': 'Self-emp-not-inc', 'fnlgt': 209642, 'education': 'HS-grad', 'education-num': 9, 'marital-status': 'Married-civ-spouse', 'occupation': 'Exec-managerial', 'relationship': 'Husband', 'race': 'White', 'sex': 'Male', 'capital-gain': 0, 'capital-loss': 0, 'hours-per-week': 45, 'native-country': 'United-States', 'salary': '>50K'}"})
+    r = client.post("/predict", json = {"model_path": "model/model.pkl",
+  "input_data": "{'age': 44,'workclass': 'Private','fnlgt': 326232,'education': 'Bachelors','education-num': 13,'marital-status': 'Divorced','occupation': 'Exec-managerial','relationship': 'Unmarried','race': 'White','sex': 'Male','capital-gain': 0,'capital-loss': 2547,'hours-per-week': 50,'native-country': 'United-States','salary': '>50K'}"})
     assert r.status_code == 200
     assert r.json() == {"salary": 1}
